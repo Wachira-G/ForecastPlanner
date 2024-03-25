40ebf68a174601d5de0022ac121b3b46efa4976e
aliceada96 <aliceada96@gmail.com>
2024-03-25 22:23:12 +0300

    sign up and login

--- UI/login.js
+++ UI/login.js
@@ -1,31 +1,31 @@
 document.getElementById('loginButton').addEventListener('click', function() {
-// Define the API URL
-const apiUrl = 'https://forecast-planner-b6f6e7dba956.herokuapp.com//api/v1/auth/login';
+  const apiUrl = 'https://forecast-planner-b6f6e7dba956.herokuapp.com//api/v1/auth/login';
 
-const userData = {
-  username: 'gabriel',
-  password: 'password'
-};
-
-// Make a GET request
-fetch(apiUrl,  {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(userData)
-})
-
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
+  const userData = {
+    username: 'gabriel',
+    password: 'password'
+  };
+  
+  // Make a GET request
+  fetch(apiUrl,  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userData)
   })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-  });
+  
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      window.location.href = "landing.html";
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+    });
--- UI/signup.js
+++ UI/signup.js
@@ -1,29 +1,29 @@
-const apiUrl = 'https://forecast-planner-b6f6e7dba956.herokuapp.com/api/v1/users';
-        
-        const signupForm = document.getElementById('signupForm');
-        const responseMessage = document.getElementById('response-message');
+const signupForm = document.getElementById('signupForm');
+const responseMessage = document.getElementById('response-message');
 
-        signupForm.addEventListener('submit', function (event) {
-          event.preventDefault();
+signupForm.addEventListener('submit', function (event) {
+  event.preventDefault;
 
-          const formData = new FormData(signupForm);
+  const apiUrl = 'https://forecast-planner-b6f6e7dba956.herokuapp.com/api/v1/users';
+  const formData = new FormData(signupForm);
+  
+  const requestOptions = {
+    method: 'POST',
+    body: formData.JSON.parse(),
+  }
 
-          const requestOptions = {
-            method: 'POST',
-            body: formData,
-          };
+  console.log(requestOptions.body);
 
-          fetch(apiUrl, requestOptions)
-            .then(response => {
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-              return response.text("Succesfullu Signed Up!");
-            })
-            .then(data => {
-              responseMessage.textContent = data;
-            })
-            .catch(error => {
-              console.error('Error:', error);
-            });
-        });
+  fetch(apiUrl, requestOptions)
+    .then((response) => {
+      if(!response.ok){
+        throw new Error(`HTTP error! status: ${response.status}`);
+      } else{
+        responseMessage('success', "Account created successfully!");
+        setTimeout(()=> window.location.replace("/login"), 2000);
+      }
+    })
+    .catch((error) => {
+      console.log('There was a problem', error);
+    });
+});
