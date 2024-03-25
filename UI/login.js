3688edcc1062069fa4bd4ddf322832f7694e50a4
aliceada96 <aliceada96@gmail.com>
2024-03-25 15:00:42 +0300

    sign up and login

--- UI/login.js
+++ UI/login.js
@@ -1,8 +1,21 @@
+document.getElementById('loginButton').addEventListener('click', function() {
 // Define the API URL
 const apiUrl = 'https://forecast-planner-b6f6e7dba956.herokuapp.com//api/v1/auth/login';
 
+const userData = {
+  username: 'gabriel',
+  password: 'password'
+};
+
 // Make a GET request
-fetch(apiUrl)
+fetch(apiUrl,  {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(userData)
+})
+
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -14,4 +27,5 @@ fetch(apiUrl)
   })
   .catch(error => {
     console.error('Error:', error);
+  });
   });
--- UI/signup.js
+++ UI/signup.js
@@ -1,29 +1,29 @@
-const apiUrl = 'https://forecast-planner-b6f6e7dba956.herokuapp.com//api/v1/users';
+const apiUrl = 'https://forecast-planner-b6f6e7dba956.herokuapp.com/api/v1/users';
+        
+        const signupForm = document.getElementById('signupForm');
+        const responseMessage = document.getElementById('response-message');
 
-const contactForm = document.getElementById('signupForm');
-const responseMessage = document.getElementById('response-message');
+        signupForm.addEventListener('submit', function (event) {
+          event.preventDefault();
 
-contactForm.addEventListener('submit', function (event) {
-  event.preventDefault();
+          const formData = new FormData(signupForm);
 
-  const formData = new FormData(SignupForm);
+          const requestOptions = {
+            method: 'POST',
+            body: formData,
+          };
 
-  const requestOptions = {
-    method: 'POST',
-    body: formData,
-  };
-
-  fetch(apiUrl, requestOptions)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.text();
-    })
-    .then(data => {
-      responseMessage.textContent = data;
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-});
+          fetch(apiUrl, requestOptions)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              return response.text("Succesfullu Signed Up!");
+            })
+            .then(data => {
+              responseMessage.textContent = data;
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
+        });
