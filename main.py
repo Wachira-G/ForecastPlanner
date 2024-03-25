5fd467cc76d33547dd07a1495fa5ab9875213f96
Gabriel Wachira <gabbyk.com@gmail.com>
2024-03-25 23:25:25 +0300

    allow cors

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 """Main module running our app."""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from config import settings
 from database import engine, Base
@@ -18,6 +19,16 @@ def start_application() -> FastAPI:
     """Start a fastAPI application."""
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     create_tables()
+    origins = [
+        "*",  # introduces security vulnerability
+    ]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(auth_routes.router, tags=["auth"], prefix="/api/v1/auth")
     app.include_router(user_routes.router, tags=["users"], prefix="/api/v1")
     app.include_router(weather_routes.router, tags=["weather"], prefix="/api/v1")
