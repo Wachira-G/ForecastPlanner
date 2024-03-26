f615db20095f3efa0bd9e41e5d5031106ec0b201
Ogollah <kogollah2011@gmail.com>
2024-03-26 14:55:50 +0300

    :construction: Create browser history

---
+++ UI/client/src/helpers/history.js
@@ -0,0 +1,9 @@
+/**
+ * Module: history
+ * Description: Creates a browser history object using the createBrowserHistory function from the 'history' library.
+ *              This history object is used for navigation and managing browser history state in React applications.
+ */
+import { createBrowserHistory } from "history";
+
+// Create a browser history object
+export const history = createBrowserHistory();
